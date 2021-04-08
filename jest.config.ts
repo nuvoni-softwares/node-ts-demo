e147c6b467d9a6e438ec1b5fb86d999e13b5b272
Rodrigo Schieck <rodrigo@nuvoni.com.br>
2021-04-08 20:45:49 -0300

    test dev

--- jest.config.ts
+++ jest.config.ts
@@ -108,7 +108,12 @@ export default {
   // projects: undefined,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+   reporters: ["default", 
+   [ "jest-junit", { 
+     suiteName: "jest tests",
+     outputDirectory: "./test_result/junit/", 
+    } ]
+  ],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
