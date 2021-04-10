58fc9e8811872967594b5b1652e04802288b0b2f
Rodrigo Schieck <rodrigo@nuvoni.com.br>
2021-04-10 11:56:26 -0300

    contato curriculos

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import app from './app'
 
 class Server {
+    readonly emailContato = 'contato@nuvoni.com.br'
+
     start(): void {
         app.listen(app.get('port'), () => {
             console.log(`IMED is running on ${app.get('port')}`)
+            console.log(`Envie seu curriculo para ${this.emailContato}`);
         })
     }
 }
