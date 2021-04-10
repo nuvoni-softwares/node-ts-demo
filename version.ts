f378cfe7e3ffa6c96d9069a94a6444f5db168a46
mauro <mauro@nuvoni.com.br>
2021-04-10 10:30:26 -0300

    atualizacao

--- version.ts
+++ version.ts
@@ -4,6 +4,6 @@ const version = process.env.CIRCLE_BUILD_NUM || 1;
 
 const html = fs.readFileSync('public/logo.html').toString(),
     reg = new RegExp('(<div class="version">)([a-zA-Z0-9 ]*)(</div>)'),
-    new_version = html.replace(reg, `$1Version ${version}$3`);
+    new_version = html.replace(reg, `$1Deploy version ${version}$3`);
 
 fs.writeFileSync('public/logo.html', new_version)
